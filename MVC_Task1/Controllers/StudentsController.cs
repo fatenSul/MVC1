f6854f036ea6045bcf040e7c3dc7142615f72aa0
Faten Sultan <1202750@student.birzeit.edu>
2024-08-27 15:03:16 +0300

    first commit

---
+++ MVC_Task1/Controllers/StudentsController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC_Task1.Controllers
+{
+    public class StudentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult GetAll(int id)
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public IActionResult Update()
+        {
+            return View();
+        }
+
+        public IActionResult Delete()
+        {
+            return View();
+        }
+    }
+}
